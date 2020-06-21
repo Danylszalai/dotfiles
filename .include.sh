32ff80772bfb73f6e64c4ef64dcdefb510cd5d95
danyl <danylszalai@gmail.com>
2020-06-21 21:15:44 +0200

    Initial commit

---
+++ .include.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DOT_DIR=$(dirname $BASH_SOURCE)
+
+. "${DOT_DIR}/.git-prompt.sh"
+
+export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1 " (%s)") \$ '
