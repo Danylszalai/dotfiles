5c7486508b962051769db30d20c7ec696e097df0
danyl <danylszalai@gmail.com>
2020-06-21 22:17:17 +0200

    Removed demo lines

--- .include.sh
+++ .include.sh
@@ -4,9 +4,4 @@ DOT_DIR=$(dirname $BASH_SOURCE)
 
 . "${DOT_DIR}/.git-prompt.sh"
 
-export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1 " (%s)") \$ '
-
-echo "Welcome to my prompt!"
-echo "Hello world!"
-echo "Welcome to my world!"
-
+export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@pawsystem\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1 " (%s)") \$ '
