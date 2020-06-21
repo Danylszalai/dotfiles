b42573f8038a1054d0061fbc602eb0b2c2df1c78
danyl <danylszalai@gmail.com>
2020-06-21 21:25:51 +0200

    Add welcome message

--- .include.sh
+++ .include.sh
@@ -5,3 +5,5 @@ DOT_DIR=$(dirname $BASH_SOURCE)
 . "${DOT_DIR}/.git-prompt.sh"
 
 export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1 " (%s)") \$ '
+
+echo "Welcome to my prompt!"
